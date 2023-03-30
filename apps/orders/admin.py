6522034c0bd7884c3ade3ca60907c8b0a3c062e5
Abayomi Olowu <aayobam@gmail.com>
2023-03-30 23:23:33 +0100

    Update admin.py

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -4,6 +4,6 @@ from .models import Order
 
 @admin.register(Order)
 class AdminOrder(admin.ModelAdmin):
-    list_display = ('id', 'order_by', 'first_name', 'last_name', 'email', 'phone_no', 'paid_amount', 'reference')
+    list_display = ('order_by', 'first_name', 'last_name', 'email', 'phone_no', 'paid_amount', 'reference')
     search_field = ('product', 'reference', 'order_by', 'email')
     readonly_fields = ["reference", 'order_by']
